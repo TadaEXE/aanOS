e955ce8aeacbc206ae90e452a4df4ce1b9ccf07e
TadaEXE <36825573+TadaEXE@users.noreply.github.com>
2025-12-08 14:26:39 +0100

    It's currently not worth having libc and libc++ support

--- sysroot/usr/include/stdlib.h
+++ sysroot/usr/include/stdlib.h
@@ -1,3 +1,20 @@
 #pragma once
+#include <stddef.h>
 
-extern "C" [[noreturn]] void abort(void);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int atexit(void (*)(void));
+int atoi(const char*);
+
+char* getenv(const char*);
+
+void free(void* ptr);
+void* malloc(size_t size);
+
+void abort(void);
+
+#ifdef __cplusplus
+}
+#endif
--- sysroot/usr/include/string.h
+++ sysroot/usr/include/string.h
@@ -1,15 +1,21 @@
 #pragma once
 #include <stddef.h>
 
-extern "C" void* memset(void* dest, int ch, size_t count);
-extern "C" void* memcpy(void* __restrict__ dest, const void* __restrict__ src,
-                        size_t count);
-extern "C" void* memmove(void* __restrict__ dest, const void* __restrict__ src,
-                         size_t count);
-extern "C" void* __memcpy_chk(void* __restrict__ dest, const void* __restrict__ src,
-                              size_t len, size_t destlen);
-extern "C" void* __memmove_chk(void* __restrict__ dest, const void* __restrict__ src,
-                               size_t len, size_t destlen);
-extern "C" size_t strlen(const char* start);
-extern "C" int memcmp(const void* lhs, const void* rhs, size_t count);
-extern "C" int strncmp(const char* lhs, const char* rhs, size_t count);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* memset(void* dest, int ch, size_t count);
+void* memcpy(void* __restrict__ dest, const void* __restrict__ src, size_t count);
+void* memmove(void* __restrict__ dest, const void* __restrict__ src, size_t count);
+void* __memcpy_chk(void* __restrict__ dest, const void* __restrict__ src, size_t len,
+                   size_t destlen);
+void* __memmove_chk(void* __restrict__ dest, const void* __restrict__ src, size_t len,
+                    size_t destlen);
+size_t strlen(const char* start);
+int memcmp(const void* lhs, const void* rhs, size_t count);
+int strncmp(const char* lhs, const char* rhs, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
